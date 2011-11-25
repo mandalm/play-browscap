b785204c141d2e0728fd10e25da62f7dc77b8aa7
Shawn Hsiao <shawn.hsiao@gmail.com>
2011-11-25 16:57:52 -0500

    browscap route wiring

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,5 +8,12 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-  
+
+  def browscap() = Action { request =>
+    val ua = request.queryString.get("ua").flatMap(_.headOption)
+    ua match {
+      case None => BadRequest()
+      case Some(x) => Ok("UA is: " + x)
+    }
+  }
 }
