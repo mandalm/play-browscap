fe3f6dd81c4c140213d832aef34e4acae296ecb2
Shawn Hsiao <shawn.hsiao@gmail.com>
2011-11-26 15:44:53 -0500

    add support and test cases to allow attr lookup have default; add scaladoc

--- app/models/BrowsCapItem.scala
+++ app/models/BrowsCapItem.scala
@@ -2,11 +2,26 @@ package models
 
 import java.util.NoSuchElementException
 
-class BrowsCapItem(val name: String, val attrs: Map[String, String]) {
+class BrowsCapItem(val name: String, val attrs: Map[String, String], val default: Option[BrowsCapItem] = None) {
+
+  /** Returns attribute value in attrs given attribute name.
+    *
+    * If the attribute is not defined in this instance's attrs, use default's attrs recursively
+    */
+  def attr(name: String): String = {
+    attrs.get(name) match {
+      case Some(x) => x
+      case None => if (default.isDefined) default.get.attr(name) else throw new NoSuchElementException
+    }
+  }
 }
 
+/** Builds a BrowsCapItem object given a XML node
+  */
 object BrowsCapItem {
-  def apply(elem: scala.xml.Elem) = {
+
+  def apply(elem: scala.xml.Elem, default: Option[BrowsCapItem] = None) = {
+
     def extract_name_value(item: scala.xml.Node) = {
       val n = item.attribute("name").get.head.text
       try {
@@ -20,7 +35,7 @@ object BrowsCapItem {
 
     val name = elem.attribute("name").get.head.text
     val attrs = Map(elem \ "item" map(extract_name_value) : _*)
-    new BrowsCapItem(name, attrs)
+    new BrowsCapItem(name, attrs, default)
   }
 }
 
--- test/models/BrowseCapItemSuite.scala
+++ test/models/BrowseCapItemSuite.scala
@@ -7,7 +7,7 @@ import collection.mutable.HashMap
 
 class BrowseCapItemSuite extends FunSuite with ShouldMatchers with OneInstancePerTest {
 
-  val sampleItemXml = <browscapitem name="DefaultProperties">
+  val defaultItemXml = <browscapitem name="DefaultProperties">
                         <item name="Comments" value="DefaultProperties" />
                         <item name="Pattern" value="DefaultProperties" />
                         <item name="Browser" value="DefaultProperties" />
@@ -40,14 +40,28 @@ class BrowseCapItemSuite extends FunSuite with ShouldMatchers with OneInstancePe
                         <item name="InternalID" value="7294" />
                       </browscapitem>
 
+  val askItemXml = <browscapitem name="Ask">
+                     <item name="Parent" value="DefaultProperties" />
+                     <item name="Comments" value="Ask" />
+                     <item name="Pattern" value="Ask" />
+                     <item name="Browser" value="Ask" />
+                     <item name="Frames" value="true" />
+                     <item name="IFrames" value="true" />
+                     <item name="Tables" value="true" />
+                     <item name="Crawler" value="true" />
+                     <item name="MasterParent" value="true" />
+                     <item name="SortOrder" value="100" />
+                     <item name="InternalID" value="4163" />
+                   </browscapitem>
+
   test("browscapitem has name") {
-    val item = models.BrowsCapItem(sampleItemXml)
+    val item = models.BrowsCapItem(defaultItemXml)
     item.name should not be (None)
     item.name should equal ("DefaultProperties")
   }
 
   test("browscapitem has attributes") {
-    val item = models.BrowsCapItem(sampleItemXml)
+    val item = models.BrowsCapItem(defaultItemXml)
     val attrs = item.attrs
     attrs should not be (None)
 
@@ -57,5 +71,19 @@ class BrowseCapItemSuite extends FunSuite with ShouldMatchers with OneInstancePe
 
     // "Platform" -> "", not None
     attrs("Platform") should be ("")
+  }
+
+  test("browscapitem with default") {
+    var default = models.BrowsCapItem(defaultItemXml)
+    val ask = models.BrowsCapItem(askItemXml, Some(default))
+
+    default.attr("Browser") should be ("DefaultProperties")
+    ask.attr("Browser") should be ("Ask")
+
+    default.attr("SortOrder") should be ("1")
+    ask.attr("SortOrder") should be ("100")
+
+    default.attr("Version") should be ("0")
+    ask.attr("Version") should be ("0")
   }
 }
