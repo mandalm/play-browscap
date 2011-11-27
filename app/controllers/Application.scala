52fd79ff7bf961c22547c3d9bf4c87c25ee1bfa8
Shawn Hsiao <shawn.hsiao@gmail.com>
2011-11-27 10:39:54 -0500

    implement browscapitem and masterparent method

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,7 +3,11 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import models._
+
 object Application extends Controller {
+
+  val browsCap = BrowsCap()
   
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
@@ -14,6 +18,20 @@ object Application extends Controller {
     ua match {
       case None => BadRequest()
       case Some(x) => Ok("UA is: " + x)
+    }
+  }
+
+  def browscapitem(id: Int) = Action {
+    browsCap.getByInternalID(id) match {
+      case None => NotFound
+      case Some(x) => Ok(views.html.item(x))
+    }
+  }
+
+  def masterparent(name: String) = Action {
+    browsCap.getMasterParentByName(name) match {
+      case None => NotFound
+      case Some(x) => Ok(views.html.item(x))
     }
   }
 }
